1d9420512bc522f86d30bf6751cbeee3636c72ef
devJay <adebajohn@gmail.com>
2026-02-22 13:47:29 +0100

    configuration for puppeteer

--- src/lib/pdf.ts
+++ src/lib/pdf.ts
@@ -32,16 +32,28 @@
 // }
 
 import chromium from "@sparticuz/chromium";
-import puppeteer from "puppeteer-core";
+import puppeteerCore from "puppeteer-core";
+import puppeteer from "puppeteer";
 
 export async function generatePDF(html: string) {
-  const executablePath = await chromium.executablePath();
+  const isProduction = process.env.VERCEL === "1";
 
-  const browser = await puppeteer.launch({
-    args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath,
-    headless: true,
-  });
+  let browser;
+
+  if (isProduction) {
+    const executablePath = await chromium.executablePath();
+
+    browser = await puppeteerCore.launch({
+      args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath,
+      headless: true,
+    });
+  } else {
+    // Local development
+    browser = await puppeteer.launch({
+      headless: true,
+    });
+  }
 
   const page = await browser.newPage();
 
